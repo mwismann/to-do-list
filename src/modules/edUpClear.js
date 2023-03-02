a84f1478eeb6096c2bcfc7a3e5b1036f32afb34c
Matt <mwbertolucci@gmail.com>
2023-03-02 17:49:08 -0300

    Add testing part 2 setup

---
+++ src/modules/edUpClear.js
@@ -0,0 +1,24 @@
+import { Save, Load } from './localStorage.js';
+
+export const editTask = (tasks, index, value) => {
+  tasks[index].description = value;
+	Save(tasks);
+}
+
+export const updateStatus = (tasks, index, checked) => {
+  tasks[index].completed = checked;
+	Save(tasks);
+};
+
+export const clearCompleted = (tasks, updateList) => {
+  const filteredTasks = tasks.filter((task) => task.completed !== true);
+	let i = 1;
+
+  filteredTasks.forEach((task) => {
+    task.index = i;
+    i += 1;
+  });
+
+	Save(filteredTasks);
+	updateList();
+};
